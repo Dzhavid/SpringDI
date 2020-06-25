375b375b1c2ac10dfe07970f59df7ade2c64f0d4
Dzhavid <eldarov.dzhavid@mail.ru>
2020-06-25 18:38:39 +0300

    Initial commit

--- src/main/java/app/model/Timer.java
+++ src/main/java/app/model/Timer.java
@@ -1,10 +1,29 @@
 package app.model;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component("timer")
+//@Scope(scopeName = "singleton")
 public class Timer {
+
+    private Timer timer;
+
+
+    public Timer() {
+    }
+
 
     private Long nanoTime = System.nanoTime();
 
     public Long getTime() {
         return nanoTime;
+    }
+
+
+    public void setNanoTime(Long nanoTime) { //установим зависимость с помощью сеттера
+        this.nanoTime = nanoTime;
     }
 }
